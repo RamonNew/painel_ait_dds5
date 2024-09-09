1d236292ee4380474c0c6b5bf2a98f6d0aa3390e
RAMON DE HOLANDA NASCIMENTO <rholanda@labcetec.net>
2024-09-09 17:18:41 -0300

    Introdução ao React: Criando componentes

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Cabecalho from './components/Cabecalho';
+
+function App() {
+  return (
+    <>
+      <Cabecalho/>
+      <h1>olá mundo</h1>
+      <p>DDS 5</p>
+    </>
+  );
+}
+
+export default App;
